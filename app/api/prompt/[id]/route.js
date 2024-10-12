7c197312eb4412f3c2542697c3603dc1fd29b9f1
AK1239 <akilkhatri235@gmail.com>
2024-10-12 20:52:30 +0300

    update and delete working

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -4,9 +4,9 @@ import Prompt from "@models/prompt";
 export const GET = async (request, { params }) => {
   try {
     await connectToDB();
-    const prompts = await Prompt.findById(params.id).populate("creator");
+    const prompt = await Prompt.findById(params.id).populate("creator");
     if (!prompt) return new Response("Prompt Not Found", { status: 404 });
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return new Response(JSON.stringify(prompt), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
