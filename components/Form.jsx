5a5ce45094e2b2b3741c31dbc9ca030db7fd1a7b
AK1239 <akilkhatri235@gmail.com>
2024-10-12 19:06:59 +0300

    complete feed page

--- components/Form.jsx
+++ components/Form.jsx
@@ -26,7 +26,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
           <span className="font-satoshi font-semibold text-base text-gray-700">
             Tag <span className="font-normal">(#product, #webdevelopment, #idea)</span>
           </span>
-          <input value={post.tag} onChange={(e) => setPost({ ...post, tag: e.target.value })} placeholder="#tage" required className="form_input" />
+          <input value={post.tag} onChange={(e) => setPost({ ...post, tag: e.target.value })} placeholder="#tag" required className="form_input" />
         </label>
 
         <div className="flex-end mx-3 mb-5 gap-4">
